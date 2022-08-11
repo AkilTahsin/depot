d2cb0aa7bc7ba0c20bead31f3190c2a5ff0f945f
Akil Tahsin <akil.tahsin@gmail.com>
2022-08-11 15:08:44 +0600

    Store (no testing yet)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'hey_there/one'
+  get 'hey_there/two'
+  get 'hey_there/three'
+  root 'store#index', as: 'store_index'
   resources :products
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
