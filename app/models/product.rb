030f10ffe1a68d8a9d5455dc3d445b8d4d448808
Akil Tahsin <akil.tahsin@gmail.com>
2022-08-12 15:26:06 +0600

    Added Catalogue and basic cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,4 @@
 class Product < ApplicationRecord
-
     # validates all fields mentioned here have value (presence: true)
     validates :title, :description, :image_url, presence: true
 
@@ -16,4 +15,19 @@ class Product < ApplicationRecord
         with: %r{\.(gif|jpg|png)\z}i,
         message: 'must be a URL for GIF, JPG or PNG image :)'
     }
+
+    has_many :line_items
+
+    before_destroy :ensure_not_referenced_by_any_line_item
+
+    private
+    # ensure that there are no line items referencing this product
+
+    def ensure_not_referenced_by_any_line_item
+        unless line_items.empty?
+            errors.add(:base, 'Line Items present')
+            throw :abort
+        end
+    end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :line_items
+  resources :carts
   get 'hey_there/one'
   get 'hey_there/two'
   get 'hey_there/three'
