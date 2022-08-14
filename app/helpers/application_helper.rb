eb5e6e671b22910e8f48a3a6ca1c9d3ef4c83be0
Akil Tahsin <akil.tahsin@gmail.com>
2022-08-14 16:32:28 +0600

    Added cart to front page + Cart auto updates when product added to cart

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,10 @@ module ApplicationHelper
     def get_time_now
         @time_now = Time.now
     end
+
+    def render_if(condition, record)
+        if condition
+            render record
+        end
+    end
 end
--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -1,4 +1,65 @@
-require "test_helper"
+# require "test_helper"
+
+# class LineItemsControllerTest < ActionDispatch::IntegrationTest
+#   setup do
+#     @line_item = line_items(:one)
+#   end
+
+#   test "should get index" do
+#     get line_items_url
+#     assert_response :success
+#   end
+
+#   test "should get new" do
+#     get new_line_item_url
+#     assert_response :success
+#   end
+
+#   test "should create line_item" do
+#     assert_difference('LineItem.count') do
+#       post line_items_url, params: { product_id: products(:ruby).id }
+#     end
+#     follow_redirect!
+#     assert_select 'h2', 'Your Cart'
+#     assert_select 'td', "Programming Ruby 1.9"
+#   end
+
+#   test "should show line_item" do
+#     get line_item_url(@line_item)
+#     assert_response :success
+#   end
+
+#   test "should get edit" do
+#     get edit_line_item_url(@line_item)
+#     assert_response :success
+#   end
+
+#   test "should update line_item" do
+#     patch line_item_url(@line_item),
+#     params: { line_item: { product_id: @line_item.product_id } }
+    
+#     assert_redirected_to line_item_url(@line_item)
+#   end
+
+#   test "should destroy line_item" do
+#     assert_difference("LineItem.count", -1) do
+#       delete line_item_url(@line_item)
+#     end
+
+#     assert_redirected_to line_items_url
+#   end
+# end
+
+
+#---
+# Excerpted from "Agile Web Development with Rails 6",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material,
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose.
+# Visit http://www.pragmaticprogrammer.com/titles/rails6 for more book information.
+#---
+require 'test_helper'
 
 class LineItemsControllerTest < ActionDispatch::IntegrationTest
   setup do
@@ -20,10 +81,19 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
       post line_items_url, params: { product_id: products(:ruby).id }
     end
 
+    test "should create line_item via ajax" do
+      assert_difference('LineItem.count') do
+        post line_items_url, params: {product_id: products(:ruby).id},
+        xhr: true
+      end
+      assert_response :suceess
+      assert_match /<tr class=\\ "line-item-highlight"/, @response.body
+    end
+
     follow_redirect!
-    
-    assert_select 'h2', 'Your Pragmatic Cart'
-    assert_select 'li', "1 \u00D7 Programming Ruby 1.9"
+
+    assert_select 'h2', 'Your Cart'
+    assert_select 'td', "Programming Ruby 1.9"
   end
 
   test "should show line_item" do
@@ -38,13 +108,12 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
 
   test "should update line_item" do
     patch line_item_url(@line_item),
-    params: { line_item: { product_id: @line_item.product_id } }
-    
+      params: { line_item: { product_id: @line_item.product_id } }
     assert_redirected_to line_item_url(@line_item)
   end
 
   test "should destroy line_item" do
-    assert_difference("LineItem.count", -1) do
+    assert_difference('LineItem.count', -1) do
       delete line_item_url(@line_item)
     end
 
